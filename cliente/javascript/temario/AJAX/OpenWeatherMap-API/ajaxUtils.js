3f653d1c58aa30f9da331f1507c9327e81f23ac1
Chirili <chirili503@gmail.com>
2020-11-03 21:48:06 +0100

    updated OpenWeatherMap

--- cliente/javascript/temario/AJAX/OpenWeatherMap-API/ajaxUtils.js
+++ cliente/javascript/temario/AJAX/OpenWeatherMap-API/ajaxUtils.js
@@ -3,9 +3,11 @@ function resolveAjax(method, url,callback){
 
     pAjax.open(method,url);
     pAjax.send();
-
+    let imagen = '<img src="./espera.gif" id="gif">'
+    document.querySelector('#cargando').innerHTML = imagen;
     pAjax.onreadystatechange = () =>{
-        if(pAjax.readyState == 4){  
+        if(pAjax.readyState == 4){
+            document.querySelector('#gif').remove();
            callback(JSON.parse(pAjax.responseText));
         }
     }
